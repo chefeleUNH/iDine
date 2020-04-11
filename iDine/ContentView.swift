bc559844f1e0ccc1ac10c755f4645df91f31ec29
chefeleUNH <chefele@newhaven.edu>
2020-04-10 22:32:14 -0400

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Charles Hefele on 4/10/20.
+//  Copyright © 2020 Charles Hefele. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
