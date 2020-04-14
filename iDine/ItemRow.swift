4cfdcf56fcc3e3eac497b980c0df4dbd40c3acb7
chefeleUNH <chefele@newhaven.edu>
2020-04-14 01:08:58 -0400

    finished the tutorial series

---
+++ iDine/ItemRow.swift
@@ -0,0 +1,47 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by Charles Hefele on 4/10/20.
+//  Copyright © 2020 Charles Hefele. All rights reserved.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    static let colors: [String: Color] = ["D": .purple, "G": .orange, "N": .red, "S": .blue, "V": .green]
+    var item: MenuItem
+    
+    var body: some View {
+        NavigationLink(destination: ItemDetail(item: item)) {
+            HStack {
+                Image(item.thumbnailImage)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.gray, lineWidth: 2))
+                VStack(alignment: .leading) {
+                    Text(item.name)
+                        .font(.headline)
+                    Text("$\(item.price)")
+                }.layoutPriority(1)
+                
+                Spacer()
+                
+                ForEach(item.restrictions, id: \.self) { restriction in
+                    Text(restriction)
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(5)
+                        .background(Self.colors[restriction, default: .black])
+                        .clipShape(Circle())
+                        .foregroundColor(.white)
+                }
+            }
+        }
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow(item: MenuItem.example)
+    }
+}
